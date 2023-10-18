80f793c4ea639c12070e4b93616b2767637bba2f
NataliaCancinoV <nataliacancinovega@hotmail.com>
2023-10-18 14:05:09 -0600

    Practica08

--- Practica08/Formulario.jsx
+++ Practica08/Formulario.jsx
@@ -7,8 +7,7 @@ function Formulario() {
         <>
             <form action="">
                 {/*<MiFieldSet titulo="Datos Personales" txt1="Nombre" txt2=" Password" />
-                <MiFieldSet titulo="Datos Escolares" txt1="Carrera" txt2=" Semestre" />
-                */}
+                <MiFieldSet titulo="Datos Escolares" txt1="Carrera" txt2=" Semestre" />*/}
                 <DatosPersonales></DatosPersonales>
                 <DatosEscolares></DatosEscolares>
                 <input type="submit" value="Enviar Datos" />
