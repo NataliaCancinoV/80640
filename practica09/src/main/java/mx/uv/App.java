da46f4f7045d5d2af796fef3cc26bd5c183c6502
NataliaCancinoV <nataliacancinovega@hotmail.com>
2023-10-19 12:55:05 -0600

    Practica09

--- practica09/src/main/java/mx/uv/App.java
+++ practica09/src/main/java/mx/uv/App.java
@@ -1,5 +1,7 @@
 package mx.uv;
 
+import static spark.Spark.*;
+
 /**
  * Hello world!
  *
@@ -9,5 +11,26 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hola Mundo!" );
+        get("/", 
+            (request, response) -> "<h1>Hola mundo!</h1>"
+        );
+        
+        get("/ruta1", 
+            (request, response) -> "<h1>Adios mundo!</h1>"
+        );
+
+        get("/ruta2", 
+            (request, response) -> "<h1>Que tal mundo!</h1>"
+        );
+        
+        get("/ruta3", 
+            (request, response) -> "<h1>Ok mundo!</h1>"
+        );
+
+        get("/ejemplo", 
+            (request, response) -> "{'alumno':'john':'matricula':'20001':'carrera':'tc'}"
+        );
+
+            
     }
 }
